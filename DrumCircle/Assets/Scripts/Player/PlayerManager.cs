1db9b6f216436ca00e636a0fed0e642d39803db0
richie <richie@ralphvr.com>
2016-12-02 15:23:19 -0500

    Added a test case of spawning a sphere at player position over network. Not sure why many of the files in the commit are being added?!? Maybe the project is not setup for git correctly within Unity?

---
+++ DrumCircle/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+//Manager Class for the Player. More info here later
+
+public class PlayerManager : NetworkBehaviour
+{
+    public GameObject AVEffect_TestVariable;
+
+    //Test case: Add movement in update and test it works across the network
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            float x = Input.GetAxis("Horizontal") * 0.1f;
+            float z = Input.GetAxis("Vertical") * 0.1f;
+
+            transform.Translate(x, 0, z);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //If player presses Space key down, will spawn sphere at their position. To test, can follow instructions here: https://docs.unity3d.com/Manual/UNetSetup.html
+                CmdSpawnTestAVEffect(); 
+            }
+        }
+    }
+
+    //Called when a player connects. Use for initialization
+    public override void OnStartLocalPlayer()
+    {
+        
+    }
+
+    [Command]
+    void CmdSpawnTestAVEffect()
+    {
+        GameObject temp = (GameObject)Instantiate(AVEffect_TestVariable, transform.position, transform.rotation);
+        temp.GetComponent<AVEffect_Sphere_1>().SpawnTime = System.DateTime.UtcNow.Millisecond; //Need to have a clock, either on server, or system time. 
+        NetworkServer.Spawn(temp);
+    }
+
+    [ClientRpc]
+    void RpcTestCall()
+    {
+        
+    }
+}
---
+++ DrumCircle/Assets/Scripts/World/AVEffect_Sphere_1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class AVEffect_Sphere_1 : NetworkBehaviour
+{
+    [SyncVar]
+    public int SpawnTime;
+
+    public float lifeTime;
+    float timeElapsed;
+
+    Vector3 startPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timeElapsed = 0f + (System.DateTime.UtcNow.Millisecond - SpawnTime) / 1000f; //use spawn time to set current elapsed time for object to be correct
+        startPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        timeElapsed += Time.fixedDeltaTime;
+
+        transform.position = startPosition + Vector3.up * timeElapsed;
+
+        if (timeElapsed > lifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    
+}
